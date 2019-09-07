7a3486bd302ef8893de99c3b5a958fc0b55bcdae
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-09-07 17:28:25 +0200

    Knex file is prepared to connect the MySQL

--- src/server/knexfile.js
+++ src/server/knexfile.js
@@ -8,8 +8,8 @@ module.exports = {
       host: process.env.DB_HOST || "127.0.0.1",
       port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "pass",
-      database: process.env.DB_NAME || "hack-your-future"
+      password: process.env.DB_PASSWORD || "password",
+      database: process.env.DB_NAME || "optimum_voyage_db"
     },
     pool: { min: 0, max: 7 },
     seeds: {
