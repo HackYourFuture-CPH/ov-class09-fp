30ef45d24c0e760136dacd1034f6ae447401723d
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-09-08 15:38:25 +0200

    updated ;)

--- src/server/api/lib/middleware/token.validator.js
+++ src/server/api/lib/middleware/token.validator.js
@@ -4,12 +4,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY;
 
 module.exports = function(req, res, next) {
-<<<<<<< HEAD
   const token = req.body.token || req.query.token || req.headers["x-access-token"];
-=======
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
->>>>>>> 4b0e39357dcc57324eb1506d9f92fe56f230139e
 
   if (token) {
     jwt.verify(token, secret, function(err, decoded) {
