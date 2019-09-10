ebfdac9c136e99c56654be526174c8feecaf8bd0
Zaki Wasik <zakiwa@gmail.com>
2019-09-10 21:28:10 +0200

    Fix authorizeUser.

--- src/server/api/routes/port.router.js
+++ src/server/api/routes/port.router.js
@@ -7,7 +7,7 @@ const router = express.Router({
   mergeParams: true
 });
 
-const { authorizePort } = require("../lib/middleware/auth.middleware");
+const { authorizeUser } = require("../lib/middleware/auth.middleware");
 
 const ROLES = require("../../constants/roles");
 
@@ -16,7 +16,7 @@ const portController = require("../controllers/port.controller");
 // ENDPOINT: /api/ports/:id :GET
 router.get(
   "/:id",
-  authorizePort(ROLES.SUPER_USER, ROLES.ADMIN, ROLES.OPERATOR),
+  authorizeUser(ROLES.SUPER_USER, ROLES.ADMIN, ROLES.OPERATOR),
   function(req, res, next) {
     portController
       .getPortById(req.params.id)
