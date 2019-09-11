f5ad514c60a08e7da1201f16472eae73ebdf085d
lil2400 <il.memedi@gmail.com>
2019-09-11 22:53:42 +0200

    creation of endpoint on organization router

---
+++ src/server/api/controllers/organization.controller.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const HttpError = require("../lib/utils/http-error");
+const knex = require("../../config/db");
+
+const createOrganization = async ({ body }) => {
+  const { name } = body;
+  const organization = await knex
+    .from("organizations")
+    .select("*")
+    .where({
+      name
+    });
+  if (organization.length !== 0) {
+    throw new HttpError("Bad request", "organization already exists!", 409);
+  }
+  return knex("organizations").insert({
+    name: body.name,
+    logo: body.logo
+  });
+};
+
+const getOrganizations = async req => {
+  try {
+    return await knex("organizations").select("id", " name", "logo");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getOrganizationsById = async id => {
+  try {
+    const organization = await knex("organizations")
+      .select(
+        "organizations.id as id",
+        "name",
+        "logo",
+        "created_at",
+        "updated_at"
+      )
+      .where({ id: id });
+    if (organization.length === 0) {
+      throw new HttpError(
+        "Bad request",
+        `Cannot find organization with ID ${id}!`,
+        404
+      );
+    }
+    return organization;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+const updateOrganizationById = async ({ body, id }) => {
+  try {
+    // const organization = await knex("organizations")
+    //     .where({ id: id })
+    //      .update({ name: body.name, logo: body.logo });
+    const organization = await knex
+      .from("organizations")
+      .select("*")
+      .where({
+        id: id
+      });
+    if (organization.length === 0) {
+      throw new HttpError(
+        "Bad request",
+        `Cannot find organization with ID ${id}!`,
+        404
+      );
+    }
+    if (body.length !== 0) {
+      return await knex("organizations")
+        .where({ id: id })
+        .update({
+          name: body.name,
+          logo: body.logo,
+          updated_at: knex.fn.now()
+        });
+    } else return "No edit fields passed, nothing updated!";
+  } catch (err) {
+    return err.message;
+  }
+};
+
+module.exports = {
+  createOrganization,
+  getOrganizations,
+  getOrganizationsById,
+  updateOrganizationById
+};
---
+++ src/server/api/routes/organizations.router.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// router setup
+
+const express = require("express");
+const router = express.Router({
+  mergeParams: true
+});
+const { authorizeUser } = require("../lib/middleware/auth.middleware");
+
+const ROLES = require("../../constants/roles");
+
+// controllers
+const organizationController = require("../controllers/organization.controller");
+
+// /api/organizations/ :POST
+router.post(
+  "/",
+  authorizeUser(ROLES.SUPER_USER, ROLES.ADMIN),
+  (req, res, next) => {
+    organizationController
+      .createOrganization({
+        body: req.body
+      })
+      .then(result =>
+        res.json({
+          message: `${req.body.name} added as new organization`,
+          id: result
+        })
+      )
+      .catch(next);
+  }
+);
+
+router.get("/", authorizeUser(ROLES.SUPER_USER), (req, res, next) => {
+  organizationController
+    .getOrganizations({
+      body: req.body
+    })
+    .then(result => res.json(result))
+    .catch(next);
+});
+
+// ENDPOINT: /api/organizations/:id :GET
+router.get(
+  "/:id",
+  authorizeUser(ROLES.SUPER_USER, ROLES.ADMIN, ROLES.OPERATOR),
+  (req, res, next) => {
+    organizationController
+      .getOrganizationsById(req.params.id)
+      .then(result => res.json(result))
+      .catch(next);
+  }
+);
+
+// ENDPOINT: /api/users/editUserRole/:id :PATCH
+router.patch(
+  "/:id",
+  // authorizeUser(ROLES.SUPER_USER),
+  (req, res, next) => {
+    organizationController
+      .updateOrganizationById({
+        body: req.body,
+        id: req.params.id
+      })
+      .then(result => res.json(result))
+      .catch(next);
+  }
+);
+
+module.exports = router;
