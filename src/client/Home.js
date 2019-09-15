8047413678522d21e4701ba0039977770d0cf40f
Niels Schmidt <niels@bytewiz.io>
2019-09-16 00:57:29 +0200

    revert changes

--- src/client/Home.js
+++ src/client/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
-import Map from "./components/Map";
 class Home extends Component {
   render() {
-    return <section>Hello</section>;
+    return <section>hello</section>;
   }
 }
 
