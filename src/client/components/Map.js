266eca368b9278537b2b23b8362581a93f76f22d
Niels Schmidt <niels@bytewiz.io>
2019-09-16 00:42:31 +0200

    make interface for map component

--- src/client/components/Map.js
+++ src/client/components/Map.js
@@ -9,36 +9,29 @@ import {
 } from "react-leaflet";
 import MapMarker from "./Marker";
 
-const waypoints = [
-  { lat: 55.721114, lng: 12.646547 },
-  { lat: 55.698478, lng: 12.604377 },
-  { lat: 55.687535, lng: 12.601538 },
-  { lat: 55.679846, lng: 12.596304 },
-  { lat: 55.677632, lng: 12.592699 }
-];
-
-const polyline = waypoints.map(({ lat, lng }) => [lat, lng]);
-
-export default class SimpleExample extends Component {
-  state = {
-    lat: 55.698478,
-    lng: 12.604377,
-    zoom: 12
-  };
-
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Polyline color="red" positions={polyline} />
-        <MapMarker position={position}>
-          <Popup>Hello from my marker! :D</Popup>
-        </MapMarker>
-      </Map>
-    );
-  }
+function MapComponent({
+  centerMapCoordinates,
+  pathColor = "red",
+  displayMarkers = false,
+  zoom = 12,
+  waypoints = []
+}) {
+  const polyline = waypoints.map(({ lat, lng }) => [lat, lng]);
+  return (
+    <Map center={centerMapCoordinates} zoom={zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Polyline color={pathColor} positions={polyline} />
+      {displayMarkers &&
+        waypoints.map(({ lat, lng, message }) => (
+          <MapMarker key={`${lat}-${lng}`} position={[lat, lng]}>
+            <Popup>{message}</Popup>
+          </MapMarker>
+        ))}
+    </Map>
+  );
 }
+
+export default MapComponent;
--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -13,4 +13,22 @@ storiesOf("Button", module)
     </Button>
   ));
 
-storiesOf("Map", module).add("MapComponent", () => <Map />);
+const centerMapCoordinates = [55.698478, 12.604377];
+const pathColor = "red";
+const displayMarkers = true;
+const waypoints = [
+  { lat: 55.721114, lng: 12.646547, waypointMessage: "waypoint 1" },
+  { lat: 55.698478, lng: 12.604377, waypointMessage: "waypoint 2" },
+  { lat: 55.687535, lng: 12.601538, waypointMessage: "waypoint 3" },
+  { lat: 55.679846, lng: 12.596304, waypointMessage: "waypoint 4" },
+  { lat: 55.677632, lng: 12.592699, waypointMessage: "waypoint 5" }
+];
+
+storiesOf("Map", module).add("MapComponent", () => (
+  <Map
+    centerMapCoordinates={centerMapCoordinates}
+    pathColor={pathColor}
+    displayMarkers={displayMarkers}
+    waypoints={waypoints}
+  />
+));
