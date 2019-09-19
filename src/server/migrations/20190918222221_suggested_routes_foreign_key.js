2c109e755c0e2157a6f6be7fa6e4e018671df70e
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-09-19 19:15:46 +0200

    suggested_routes inconsistencies: changed foreign key

--- src/server/migrations/20190918222221_suggested_routes_foreign_key.js
+++ src/server/migrations/20190918222221_suggested_routes_foreign_key.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex) {
   return knex.schema.table("suggested_routes", table => {
-    table.dropForeign("routes_voyage_id_foreign");
+    table.dropForeign("voyage_id", "routes_voyage_id_foreign");
     table
       .foreign("voyage_id")
       .references("voyages.id")
@@ -11,4 +11,13 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  return knex.schema.table("suggested_routes", table => {
+    table.dropForeign("voyage_id");
+    table
+      .foreign("voyage_id", "routes_voyage_id_foreign")
+      .references("voyages.id")
+      .onDelete("CASCADE")
+      .onUpdate("RESTRICT");
+  });
+};
