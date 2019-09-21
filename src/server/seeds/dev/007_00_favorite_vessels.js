a5b8723b8ce1b0d6f3757bbbd52a3aefcc6c020e
Zaki Wasik <zakiwa@gmail.com>
2019-09-21 21:57:23 +0200

    Reorder seed files and fix minor issues

--- src/server/seeds/dev/007_00_favorite_vessels.js
+++ src/server/seeds/dev/007_00_favorite_vessels.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("favourite_vessels")
+  return knex("favorite_vessels")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("favourite_vessels").insert([
+      return knex("favorite_vessels").insert([
         {
           user_id: 1,
           vessel_id: 1
