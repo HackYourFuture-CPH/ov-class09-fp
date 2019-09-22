e263b8417357736171bfdac233eb3be3c1d70a0f
Omid-Incarnation <42556054+Omid-Incarnation@users.noreply.github.com>
2019-09-22 18:14:31 +0200

    Vessel reports.position waypoint (#55)
    * vessel reports "position" column renamed ;)
    
    * "position_waypoint" column is modified ;)
    
    * "position_waypoint" is a foreign "waypoints.id"
    
    * vessel_reports seed is modyfied ;)

--- src/server/seeds/dev/013_00_vessel_reports.js
+++ src/server/seeds/dev/013_00_vessel_reports.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           vessel_id: 1,
-          position: 5,
+          position_waypoint: 1,
           current_speed: 234.44,
           hfo_consumption: 212.22,
           lsfo_consumption: 3445.55
@@ -16,15 +16,15 @@ exports.seed = function(knex, Promise) {
         {
           id: 2,
           vessel_id: 2,
-          position: 3,
+          position_waypoint: 2,
           current_speed: 234.44,
           hfo_consumption: 212.22,
           lsfo_consumption: 3445.55
         },
         {
           id: 3,
-          vessel_id: 1,
-          position: 4,
+          vessel_id: 3,
+          position_waypoint: 3,
           current_speed: 234.44,
           hfo_consumption: 212.22,
           lsfo_consumption: 3445.55
