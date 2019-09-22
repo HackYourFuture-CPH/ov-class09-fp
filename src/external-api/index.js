b8d69e88af07092b0cd53822479ac61995710fff
lil2400 <il.memedi@gmail.com>
2019-09-22 15:57:52 +0200

    Removed json-server from dev script

--- src/external-api/index.js
+++ src/external-api/index.js
@@ -2,12 +2,14 @@ const routeInfo = require("./data/route_info_dump.json");
 const vesselList = require("./data/vessel_list.json");
 const vesselReport = require("./data/vessel_report_dump.json");
 const voyageInfo = require("./data/voyage_info.json");
+const companyList = require("./data/company_list.json");
 
 module.exports = function() {
   return {
     route_info: routeInfo,
     vassel_list: vesselList,
     vessel_report: vesselReport,
-    voyage_info: voyageInfo
+    voyage_info: voyageInfo,
+    company_list: companyList
   };
 };
