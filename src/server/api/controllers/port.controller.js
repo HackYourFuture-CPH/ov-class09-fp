9be0cd6665cf6463b938e756392362673ce804dd
justinaoyov <justina.oyovwevotu@gmail.com>
2019-09-22 15:52:57 +0200

    port post endpoint extended for waypoint data

--- src/server/api/controllers/port.controller.js
+++ src/server/api/controllers/port.controller.js
@@ -18,7 +18,7 @@ const getPortById = async id => {
 };
 
 const createPort = async ({ body }) => {
-  const { name } = body;
+  const { name, sailtime, route_id, waypoints } = body;
 
   if (name.length === 0) {
     throw new HttpError("Bad request", "Port name is missing!", 409);
@@ -31,13 +31,52 @@ const createPort = async ({ body }) => {
       name
     });
 
+  const route = await knex
+    .from("suggested_routes")
+    .select("id")
+    .where({
+      id: route_id
+    });
+
   if (ports.length !== 0) {
     throw new HttpError("Bad request", "port already exists!", 409);
   } else {
-    return knex("ports").insert({
-      name: name
-    });
+    return knex("ports")
+      .insert({
+        name: name
+      })
+      .then(portID => {
+        if (route.length > 0 && waypoints) {
+          return knex("waypoints").insert({
+            port_id: portID[0],
+            route_id: route[0].id,
+            lon: waypoints[0].lon,
+            lat: waypoints[0].lat
+          });
+        } else {
+          return `No Waypoint Added for port: '${name}' `;
+        }
+      })
+      .catch(error => {
+        return `Error occured!  ${error.message}`;
+      });
   }
 };
 
 module.exports = { getPortById, createPort };
+
+/* 
+ 
+ //let new_waypoint = waypoints.map((item) => {item.lon, item.lat})
+ {
+    "name": "Port sunshine",
+    "sailtime": 22.3,
+    "waypoints": [
+        {
+            "lon": 55.65646,
+            "lat": 12.53635
+        }
+    ]
+}
+
+ */
