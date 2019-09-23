3331f67df40ccbfd02c17efad3866fd724b8f5dc
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-09-23 23:16:33 +0200

    Vessels Ui component

---
+++ src/client/components/VesselsUI.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+function VesselsUIComponent({
+  vesselName,
+  departureFrom,
+  arrivesAt,
+  etd,
+  eta
+}) {
+  return (
+    <div>
+      <h1>{vesselName}</h1>
+      <label>Departure</label>
+      <h3>{departureFrom}</h3>
+      <p>{etd}</p>
+      <label>ETA</label>
+      <h3>{arrivesAt}</h3>
+      <p>{eta}</p>
+    </div>
+  );
+}
+
+export default VesselsUIComponent;
