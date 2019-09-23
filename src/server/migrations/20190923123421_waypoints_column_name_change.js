29f1e3c5e270d62f1f5703609db9edac57d5b2ad
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-09-23 13:12:36 +0200

    suggested_routes inconsistencies: changed column name and foregin key in waypoints table

---
+++ src/server/migrations/20190923123421_waypoints_column_name_change.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.up = function(knex) {
+  return knex.schema.alterTable("waypoints", table => {
+    table.renameColumn("route_id", "suggested_route_id");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("voyages", table => {
+    table.renameColumn("suggested_route_id", "route_id");
+  });
+};
