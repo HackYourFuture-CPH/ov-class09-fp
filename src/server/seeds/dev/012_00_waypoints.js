8604da0b0d6973613d7e0ce213de7510971dfb35
Zaki Wasik <zakiwa@gmail.com>
2019-09-23 23:18:46 +0200

    Do not empty table before running this script
    This table will cleared by the port waypoints seed. Clearing the table now will remove the data added by the port waypoint seed.

--- src/server/seeds/dev/012_00_waypoints.js
+++ src/server/seeds/dev/012_00_waypoints.js
@@ -5,29 +5,27 @@ exports.seed = function(knex, Promise) {
   console.info(`Running seed file: ${scriptName}`);
 
   // Deletes ALL existing entries
-  return knex("waypoints")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("waypoints").insert([
-        {
-          id: 4,
-          suggested_route_id: 1,
-          lon: 2344.44,
-          lat: 2234.87
-        },
-        {
-          id: 5,
-          suggested_route_id: 2,
-          lon: 2344.44,
-          lat: 2234.87
-        },
-        {
-          id: 6,
-          suggested_route_id: 3,
-          lon: 2344.44,
-          lat: 2234.87
-        }
-      ]);
-    });
+  return knex("waypoints").then(function() {
+    // Inserts seed entries
+    return knex("waypoints").insert([
+      {
+        id: 4,
+        suggested_route_id: 1,
+        lon: 2344.44,
+        lat: 2234.87
+      },
+      {
+        id: 5,
+        suggested_route_id: 2,
+        lon: 2344.44,
+        lat: 2234.87
+      },
+      {
+        id: 6,
+        suggested_route_id: 3,
+        lon: 2344.44,
+        lat: 2234.87
+      }
+    ]);
+  });
 };
