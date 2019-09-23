edba68ff4623e8025c3d45b4437adfe82d238e65
Omid-Incarnation <42556054+Omid-Incarnation@users.noreply.github.com>
2019-09-23 18:26:42 +0200

    favorite vessels API is added ;) (#45)
    * favorite vessels API is added ;)
    
    * favorite_vessels API is corrected! ;)
    
    * change the post Authorization to post

---
+++ src/server/api/controllers/favorite-vessels.controller.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const HttpError = require("../lib/utils/http-error");
+const knex = require("../../config/db");
+
+//POST
+const createFavoriteVessel = async ({ body }) => {
+  const { user_id, vessel_id } = body;
+  const checkUserId = await knex
+    .from("users")
+    .select("*")
+    .where({ id: user_id });
+
+  const checkVesselsId = await knex
+    .from("vessels")
+    .select("*")
+    .where({ id: vessel_id });
+
+  if (checkUserId.length === 0 || checkVesselsId.length === 0) {
+    throw new HttpError(
+      "Bad request",
+      "user_id or vessel_id was not found!",
+      404
+    );
+  }
+
+  return knex("favorite_vessels").insert({ user_id, vessel_id });
+};
+
+//DELETE
+const deleteFavoriteVessel = ({ body }) => {
+  const { user_id, vessel_id } = body;
+  return knex("favorite_vessels")
+    .where({ user_id, vessel_id })
+    .del();
+};
+
+module.exports = {
+  createFavoriteVessel,
+  deleteFavoriteVessel
+};
