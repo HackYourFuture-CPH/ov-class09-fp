dfa65794d322a38acca25b6024e47ac8518cd476
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-09-23 22:12:51 +0200

    improved ;)

--- src/server/migrations/20190923211830_ecas_column_rename.js
+++ src/server/migrations/20190923211830_ecas_column_rename.js
@@ -2,14 +2,14 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable("ecas", table => {
-    table.renameColumn("lon", "longitude");
-    table.renameColumn("lat", "latitude");
+    table.renameColumn("lon", "longitude").alter();
+    table.renameColumn("lat", "latitude").alter();
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.alterTable("ecas", table => {
-    table.renameColumn("longitude", "lon");
-    table.renameColumn("latitude", "lat");
+    table.renameColumn("longitude", "lon").alter();
+    table.renameColumn("latitude", "lat").alter();
   });
 };
