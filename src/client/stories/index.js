23ac23f3d1aadcbd4d079c7420fffe82917313f5
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-09-24 12:49:35 +0200

    added the vessels details stories

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -3,6 +3,7 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { MapStory } from "./Map";
 import { WithTextStory, WithEmojiStory } from "./Buttons";
+import { VesselsDetailsStory } from "./VesselsDetails";
 
 storiesOf("Button", module)
   .add("with text", WithTextStory)
@@ -10,3 +11,9 @@ storiesOf("Button", module)
 
 const MapStories = storiesOf("Map", module).add("MapComponent", MapStory);
 MapStories.addDecorator(withKnobs);
+
+const VesselsDetailsStories = storiesOf("VesselsDetails", module).add(
+  "VesselsDetailsComponent",
+  VesselsDetailsStory
+);
+VesselsDetailsStories.addDecorator(withKnobs);
