ceca38c59995244596845b9bea3e6f1914e9b1f6
justinaoyov <justina.oyovwevotu@gmail.com>
2019-09-27 21:38:52 +0200

    Added Start Voyage main component to storybook

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -3,6 +3,7 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { MapStory } from "./Map";
 import { WithTextStory, WithEmojiStory } from "./Buttons";
+import { StartVoyageStory } from "./StartVoyage";
 
 storiesOf("Button", module)
   .add("with text", WithTextStory)
@@ -10,3 +11,9 @@ storiesOf("Button", module)
 
 const MapStories = storiesOf("Map", module).add("MapComponent", MapStory);
 MapStories.addDecorator(withKnobs);
+
+const VoyageStories = storiesOf("StartVoyage", module).add(
+  "StartVoyageComponent",
+  StartVoyageStory
+);
+VoyageStories.addDecorator(withKnobs);
