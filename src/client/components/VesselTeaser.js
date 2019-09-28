3bdb7dbc5cbeedc842af83cbaa287e14cba5d0bf
Swapna-Sahu <47104798+Swapna-Sahu@users.noreply.github.com>
2019-09-29 01:29:57 +0200

    Feature/vessels-ui (#88)
    * created vessels-UI components
    
    * Vessels Ui component
    
    * added Vessels UI story
    
    * added Vessels UI story
    
    * deleted the folder Vessels-UI
    
    * created VesselsList and VesselTeaser file
    
    * add json data in Vessels
    
    * created vesselTeaser story
    
    * renamed the method name

---
+++ src/client/components/VesselTeaser.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+function VesselTeaser({ id, vesselName, departureFrom, arrivesAt, etd, eta }) {
+  return (
+    <div>
+      <h1>{vesselName}</h1>
+      <label>Departure</label>
+      <h3>{departureFrom}</h3>
+      <p>{etd}</p>
+      <label>ETA</label>
+      <h3>{arrivesAt}</h3>
+      <p>{eta}</p>
+    </div>
+  );
+}
+
+export default VesselTeaser;
