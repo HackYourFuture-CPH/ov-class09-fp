c5a98a70a309424da32baef81824b2a21112bae8
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-09-28 19:27:10 +0200

    Fix the down migration with defaulting voyage_id to 1

--- src/server/migrations/20190928155135_connect-suggested-routes-with-vessel-report.js
+++ src/server/migrations/20190928155135_connect-suggested-routes-with-vessel-report.js
@@ -31,6 +31,7 @@ exports.down = function(knex) {
         .integer("voyage_id")
         .unsigned()
         .notNullable()
+        .defaultTo(1) // All the values are reset to 1 to ensure that it does not break foreign key constraint suggested_routes_voyage_id_foreign
         .references("id")
         .inTable("voyages")
         .onDelete("CASCADE")
