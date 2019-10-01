f6547d2ec76846a102dd5d0c1ed89bdeaab0df87
NayanaKamtekar <nayana.kamtekar@gmail.com>
2019-10-01 13:37:13 +0200

    Added method for checkbox and coditional display of hire rate

--- src/client/components/voyage/Checkbox.js
+++ src/client/components/voyage/Checkbox.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Checkbox extends Component {
   render() {
-    const { label } = this.props;
+    const { label, handleCheckbox } = this.props;
     return (
       <div>
         <label htmlFor="checkbox">{label}</label>
-        <input type="checkbox" id="checkbox" />
+        <input type="checkbox" id="checkbox" onClick={e => handleCheckbox(e)} />
       </div>
     );
   }
