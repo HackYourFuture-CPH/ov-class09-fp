b1c2b1b3ff4a574a60361603352d82b8453c6492
Zaki Wasik <zakiwa@gmail.com>
2019-10-03 16:02:09 +0200

    Changed selected_route column on vessel_reports to singular

--- src/server/migrations/20190930210406_add_column_selected_routes_to_vessels_report.js
+++ src/server/migrations/20190930210406_add_column_selected_routes_to_vessels_report.js
@@ -3,7 +3,7 @@
 exports.up = function(knex) {
   return knex.schema.table("vessel_reports", table => {
     table
-      .integer("selected_routes")
+      .integer("selected_route")
       .unsigned()
       .references("id")
       .inTable("suggested_routes")
@@ -14,8 +14,8 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema.alterTable("vessel_reports", table => {
-    table.dropForeign("selected_routes");
-    table.dropIndex("selected_routes");
-    table.dropColumn("selected_routes");
+    table.dropForeign("selected_route");
+    table.dropIndex("selected_route");
+    table.dropColumn("selected_route");
   });
 };
