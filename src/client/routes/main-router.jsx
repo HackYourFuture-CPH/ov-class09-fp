eb9737c8366cb7575cc21ebba00e0df2d7410c6b
justinaoyov <justina.oyovwevotu@gmail.com>
2019-10-03 12:01:18 +0200

    removed startVoyage from route in react

--- src/client/routes/main-router.jsx
+++ src/client/routes/main-router.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import { Route, withRouter, Link } from "react-router-dom";
 
 import Home from "./../Home";
-import StartVoyage from "../components/voyage/StartVoyageMain";
 
 const MainRouter = props => {
   return (
     <React.Fragment>
       <div className="container">
         <Route exact path="/" component={Home} />
-        <Route exact path="/" component={StartVoyage} />
       </div>
     </React.Fragment>
   );
