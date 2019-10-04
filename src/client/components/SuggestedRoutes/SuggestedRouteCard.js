1c58dab68fc65b1d9f0ca576ed0c3c2b8cf96bad
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-04 23:58:26 +0200

    improving the SuggestedRouteCard ;)

--- src/client/components/SuggestedRoutes/SuggestedRouteCard.js
+++ src/client/components/SuggestedRoutes/SuggestedRouteCard.js
@@ -1,14 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import SuggestedRouteDetails from "./SuggestedRouteDetails";
 import SuggestedRouteSummary from "./SuggestedRouteSummary";
 
-export default class SuggestedRouteCard extends Component {
-  render() {
-    return (
-      <div>
-        <SuggestedRouteSummary props={this.props} />
-        <SuggestedRouteDetails props={this.props} />
-      </div>
-    );
-  }
+export default function SuggestedRouteCard(props) {
+  return (
+    <div>
+      <SuggestedRouteSummary
+        time={props.time}
+        price={props.price}
+        id={props.id}
+        isSelected={props.isSelected}
+        handleClick={props.handleClick}
+      />
+      <SuggestedRouteDetails
+        eta={props.eta}
+        sailDays={props.sailDays}
+        maxWaveHs={props.maxWaveHs}
+        avgSpeed={props.avgSpeed}
+        hireCost={props.hireCost}
+        consumption={props.consumption}
+      />
+    </div>
+  );
 }
