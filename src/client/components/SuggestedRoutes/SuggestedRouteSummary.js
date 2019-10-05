eb52d9e21935d319b359218007b696b687322045
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-05 21:15:49 +0200

    change to function component ;)

--- src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
+++ src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import TitleAndDescribe from "../TitleAndDescribe";
 
-export default class SuggestedRouteSummary extends Component {
-  render() {
-    const { id, isSelected, handleClick, time, price } = this.props;
-    return (
-      <div>
-        {isSelected ? <h4>SELECTED ROUTE</h4> : null}
-        <TitleAndDescribe title="HOURS" description={time} />
-        <TitleAndDescribe title="TOTAL COST" description={price} />
-        {!isSelected ? (
-          <button onClick={() => handleClick(id)}>SELECT ROUTE</button>
-        ) : null}
-      </div>
-    );
-  }
+export default function SuggestedRouteSummary({
+  id,
+  isSelected,
+  handleClick,
+  time,
+  price
+}) {
+  return (
+    <div>
+      {isSelected ? <h4>SELECTED ROUTE</h4> : null}
+      <TitleAndDescribe title="HOURS" description={time} />
+      <TitleAndDescribe title="TOTAL COST" description={price} />
+      {!isSelected ? (
+        <button onClick={() => handleClick(id)}>SELECT ROUTE</button>
+      ) : null}
+    </div>
+  );
 }
