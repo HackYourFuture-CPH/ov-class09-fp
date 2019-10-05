dd63ddfcd30ea96af9ab7133f6bdca57282eb82a
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-05 21:03:57 +0200

    change to function component ;)

--- src/client/components/SuggestedRoutes/SuggestedRouteDetails.js
+++ src/client/components/SuggestedRoutes/SuggestedRouteDetails.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 import TitleAndDescribe from "../TitleAndDescribe";
 
-export default class SuggestedRouteDetails extends Component {
-  render() {
-    const {
-      eta,
-      sailDays,
-      maxWaveHs,
-      avgSpeed,
-      hireCost,
-      consumption
-    } = this.props;
-
-    return (
-      <React.Fragment>
-        <div>
-          <TitleAndDescribe title={"ETA"} description={eta} />
-          <TitleAndDescribe title={"SAIL DAYS"} description={sailDays} />
-          <TitleAndDescribe title={"MAX WAVE HS"} description={maxWaveHs} />
-        </div>
-        <div>
-          <TitleAndDescribe title={"AVG SPEED"} description={avgSpeed} />
-          <TitleAndDescribe title={"HIRE COST"} description={hireCost} />
-          <TitleAndDescribe title={"CONSUMPTION"} description={consumption} />
-        </div>
-      </React.Fragment>
-    );
-  }
+export default function SuggestedRouteDetails({
+  eta,
+  sailDays,
+  maxWaveHs,
+  avgSpeed,
+  hireCost,
+  consumption
+}) {
+  return (
+    <Fragment>
+      <div>
+        <TitleAndDescribe title={"ETA"} description={eta} />
+        <TitleAndDescribe title={"SAIL DAYS"} description={sailDays} />
+        <TitleAndDescribe title={"MAX WAVE HS"} description={maxWaveHs} />
+      </div>
+      <div>
+        <TitleAndDescribe title={"AVG SPEED"} description={avgSpeed} />
+        <TitleAndDescribe title={"HIRE COST"} description={hireCost} />
+        <TitleAndDescribe title={"CONSUMPTION"} description={consumption} />
+      </div>
+    </Fragment>
+  );
 }
