8b56eeeb1a3093b8a7f9f034acc8177624ae4960
Zaki Wasik <zakiwa@gmail.com>
2019-10-08 03:48:56 +0200

    Massive DB schema change. See descriptions below.
    Voyages:
    Renamed field voyages.eta to voyages.target_arrival_time
    Rename dfield voyages.etd to voyages.departure_time
    Renamed voyages.arrive_at_waypoint to voyages.arrive_at_port
    Renamed voyages.depart_from_waypoint to voyages.depart_from_port
    Removed field sailtime
    
    Vessel Reports
    Added field vessel_reports.eta
    Changed vessel_reports.vessel_id to vessel_reports.voyage_id
    Added vessel_reports.latitude
    Added vessel_reports.longitude
    Removed vessel_reports.position
    
    Ports
    Added ports.depth
    
    Waypoints
    Added waypoints.speed

--- src/server/migrations/Schema.sql
+++ src/server/migrations/Schema.sql
@@ -1,33 +1,34 @@
 CREATE TABLE `organizations` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) UNIQUE NOT NULL,
+  `name` varchar(100) UNIQUE NOT NULL,
   `logo` text DEFAULT null,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now())
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `ecas` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `longitude` decimal NOT NULL,
-  `latitude` decimal NOT NULL,
-  `name` varchar(255) NOT NULL
+  `longitude` decimal(11, 8) NOT NULL,
+  `latitude` decimal(10, 8) NOT NULL,
+  `name` varchar(50) NOT NULL
 );
 
 CREATE TABLE `ports` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `latitude` decimal NOT NULL,
-  `longitude` decimal NOT NULL,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now())
+  `name` varchar(100) NOT NULL,
+  `latitude` decimal(10, 8) NOT NULL,
+  `longitude` decimal(11, 8) NOT NULL,
+  `depth` decimal NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `user_roles` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `role` varchar(255) NOT NULL,
+  `role` varchar(30) NOT NULL,
   `status` tinyint NOT NULL,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now())
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `users` (
@@ -35,19 +36,20 @@ CREATE TABLE `users` (
   `role_id` int NOT NULL,
   `email` varchar(255) UNIQUE NOT NULL,
   `organization_id` int NOT NULL,
-  `passowrd` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `status` tinyint,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now()),
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `session_token` text DEFAULT null,
   `refreshing_token` text DEFAULT null
 );
 
 CREATE TABLE `favorite_vessels` (
-  `user_id` int PRIMARY KEY NOT NULL,
+  `user_id` int NOT NULL,
   `vessel_id` int NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT (now())
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`, `vessel_id`)
 );
 
 CREATE TABLE `suggested_routes` (
@@ -60,83 +62,78 @@ CREATE TABLE `suggested_routes` (
   `distance_over_ground` float NOT NULL,
   `distance_through_water` float NOT NULL,
   `avgspeed` float NOT NULL,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now()),
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `vessel_report_id` int NOT NULL
 );
 
 CREATE TABLE `vessels` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `organization_id` int NOT NULL,
-  `mmsi` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `created_at` timestamp DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now())
+  `mmsi` varchar(9) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `vessel_reports` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `voyage_id` int NOT NULL,
-  `position_waypoint` int DEFAULT null,
+  `latitude` decimal(10, 8) NOT NULL,
+  `longitude` decimal(11, 8) NOT NULL,
+  `eta` timestamp, 
   `current_speed` float NOT NULL,
   `hfo_consumption` float NOT NULL,
   `lsfo_consumption` float NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `selected_route_id` int DEFAULT null
 );
 
 CREATE TABLE `voyages` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `vessel_id` int NOT NULL,
-  `etd` datetime DEFAULT null,
-  `eta` datetime DEFAULT null,
+  `departure_time` datetime DEFAULT null,
+  `target_arrival_time` datetime DEFAULT null,
   `forwarddraft` float NOT NULL,
   `aftdraft` float NOT NULL,
-  `optimisation_type` ENUM ('a','b') NOT NULL,
+  `optimisation_type` ENUM ("earliest_arrival", "fixed_eta", "least_cost_arrival") DEFAULT "earliest_arrival",
   `depart_from_port` int DEFAULT null,
   `arrive_at_port` int DEFAULT null,
-  `sailtime` float NOT NULL DEFAULT 0,
-  `hfocost` float NOT NULL DEFAULT 0,
-  `lsfocost` float NOT NULL DEFAULT 0,
-  `hirerate` float NOT NULL,
-  `status` ENUM ('a','b',"created") NOT NULL DEFAULT "created",
-  `created_at` timestamp NOT NULL DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now())
+  `hfocost` float DEFAULT 0,
+  `lsfocost` float DEFAULT 0,
+  `hirerate` float DEFAULT 0,
+  `status` ENUM ("created", "ongoing", "completed") NOT NULL DEFAULT "created",
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `waypoints` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `suggested_route_id` int,
-  `longitude` decimal NOT NULL,
-  `latitude` decimal NOT NULL,
-  `rpm` decimal,
-  `created_at` timestamp NOT NULL DEFAULT (now()),
-  `updated_at` timestamp NOT NULL DEFAULT (now()),
+  `longitude` decimal(11, 8) NOT NULL,
+  `latitude` decimal(10, 8) NOT NULL,
+  `speed` decimal NOT NULL,
+  `rpm` decimal NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `sequence_id` int DEFAULT null
 );
 
-ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`);
-
-ALTER TABLE `voyages` ADD FOREIGN KEY (`vessel_id`) REFERENCES `vessels` (`id`);
+ALTER TABLE `favorite_vessels` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `favorite_vessels` ADD FOREIGN KEY (`vessel_id`) REFERENCES `vessels` (`id`);
 
 ALTER TABLE `suggested_routes` ADD FOREIGN KEY (`vessel_report_id`) REFERENCES `vessel_reports` (`id`);
 
-ALTER TABLE `vessel_reports` ADD FOREIGN KEY (`selected_route_id`) REFERENCES `suggested_routes` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`id`);
 
-ALTER TABLE `waypoints` ADD FOREIGN KEY (`suggested_route_id`) REFERENCES `suggested_routes` (`id`);
+ALTER TABLE `vessel_reports` ADD FOREIGN KEY (`selected_route_id`) REFERENCES `suggested_routes` (`id`);
+ALTER TABLE `vessel_reports` ADD FOREIGN KEY (`voyage_id`) REFERENCES `voyages` (`id`);
 
 ALTER TABLE `vessels` ADD FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`);
 
-ALTER TABLE `vessel_reports` ADD FOREIGN KEY (`voyage_id`) REFERENCES `voyages` (`id`);
-
-ALTER TABLE `voyages` ADD FOREIGN KEY (`depart_from_port`) REFERENCES `ports` (`id`);
-
 ALTER TABLE `voyages` ADD FOREIGN KEY (`arrive_at_port`) REFERENCES `ports` (`id`);
+ALTER TABLE `voyages` ADD FOREIGN KEY (`depart_from_port`) REFERENCES `ports` (`id`);
+ALTER TABLE `voyages` ADD FOREIGN KEY (`vessel_id`) REFERENCES `vessels` (`id`);
 
-ALTER TABLE `vessel_reports` ADD FOREIGN KEY (`position_waypoint`) REFERENCES `waypoints` (`id`);
-
-ALTER TABLE `favorite_vessels` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `favorite_vessels` ADD FOREIGN KEY (`vessel_id`) REFERENCES `vessels` (`id`);
+ALTER TABLE `waypoints` ADD FOREIGN KEY (`suggested_route_id`) REFERENCES `suggested_routes` (`id`);
