8d6e777f9b44f4b006c52028c4722faacc03431a
Rahim <ismailirahim6@gmail.com>
2019-10-09 22:01:57 +0200

    created story for AdminPage components

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -5,6 +5,10 @@ import { MapStory } from "./Map";
 import { WithTextStory, WithEmojiStory } from "./Buttons";
 import { VesselsListStory } from "./Vessels";
 import { VesselTeaserStory } from "./VesselTeaser";
+import { UserDetailStory } from "./UserDetail";
+import { CreateUserStory } from "./CreateUser";
+import { ManageUsersTableStory } from "./ManageUsersTable";
+import { AdminPageStory } from "./AdminPage";
 
 storiesOf("Button", module)
   .add("with text", WithTextStory)
@@ -17,3 +21,9 @@ const VesselsListStories = storiesOf("Vessels", module)
   .add("VesselList", VesselsListStory)
   .add("Vessel Teaser", VesselTeaserStory);
 VesselsListStories.addDecorator(withKnobs);
+const UserStories = storiesOf("Admin Page Components", module)
+  .add("UserDetail", UserDetailStory)
+  .add("CreateUser", CreateUserStory)
+  .add("ManageUsersTable", ManageUsersTableStory)
+  .add("AdminPage", AdminPageStory);
+UserStories.addDecorator(withKnobs);
