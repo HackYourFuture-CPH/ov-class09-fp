075c83ee7f01503e2c87ec62363a362a52c860a7
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-12 13:49:24 +0200

    improving the Field Component ;)

--- src/client/components/Field.js
+++ src/client/components/Field.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Field({ title, description }) {
   return (
     <div>
-      <h4>{title}</h4>
-      <h4>{description}</h4>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   );
 }
