810e369394e962342379508e40139e97c37f114b
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-10-17 13:51:35 +0200

    dont merge

--- src/client/components/VoyageDetailsContainer.js
+++ src/client/components/VoyageDetailsContainer.js
@@ -18,29 +18,34 @@ export default class VoyageDetailsContainer extends Component {
       headers: {
         "Content-Type": "application/json",
         authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InN1cGVydXNlciIsImlhdCI6MTU3MTI1MjM3NCwiZXhwIjoxNTcxMjg4Mzc0fQ.iYsferWTr1pKoXXtUdhF3N52cHFaYUBjUllK2OxeA7k"
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InN1cGVydXNlciIsImlhdCI6MTU3MTMwNDM4MywiZXhwIjoxNTcxMzQwMzgzfQ.uRHGFpSjYx4WVB4Tv3AcD94vStfV9VDsIMVAd6JECiI"
       }
     };
     console.log("Inside the component didcomponent");
     axios
       .get(`/api/voyages/${this.state.voyageId}/vessel-reports`, headerObject)
       .then(data => (data = data.data))
-      .then(data =>
-        this.setState({
-          vessel_report: data,
-          vessel_report_id: data.map(({ id }) => id)
-        })
+      .then(
+        data => console.log(data[0].id)
+        //this.setState({
+        //  vessel_report: data,
+        //  vessel_report_id: data[0].id
+        //}),
       );
-
+    console.log(this.state.vessel_report_id);
     axios
-      .get("/api/vessel-reports/956/suggested-routes", headerObject)
+      .get(
+        `/api/vessel-reports/${this.state.vessel_report_id}/suggested-routes`,
+        headerObject
+      )
       .then(data => (data = data.data))
-      .then(data =>
+      .then(data => {
         this.setState({
-          suggested_routes: data,
-          suggested_routes_id: data.map(({ id }) => id)
-        })
-      );
+          suggested_routes: data
+          // suggested_route_id: data.id,
+          //suggested_routes_id: data.map(({ id }) => id)
+        });
+      });
 
     /*  
      axios
@@ -59,7 +64,7 @@ export default class VoyageDetailsContainer extends Component {
   }
   render() {
     console.log(this.state);
-    //console.log(this.state.vessel_report_id[0]);
+    // console.log(this.state.vessel_report_id);
 
     /* vesselName,-voyages
     departsFrom,-voyages
@@ -88,31 +93,7 @@ export default class VoyageDetailsContainer extends Component {
     */
     return (
       <div>
-        <VoyageDetails
-          vesselName={vesselName}
-          departureFrom={departsFrom}
-          arrivesAt={arrivesAt}
-          etd={etd}
-          eta={eta}
-          date={date}
-          latitude={latitude}
-          longitude={longitude}
-          hsfo={hsfo}
-          ulsfo={ulsfo}
-        />
-
-        <CostWidget
-          totalCost={number("Total Cost", 200)}
-          hfoCost={number("hfo Cost", 10)}
-          lsfoCost={number("lsfo Cost", 10)}
-          hfoConsumption={number("hfo Consumption", 10)}
-          lsfoConsumption={number("lsfo Consumption", 10)}
-        />
-        <SuggestedRouteTable
-          data={data}
-          tableNames={["DATE", "LATITUDE", "LONGTIDUE", "SPEED", "EST.RPM"]}
-        />
-        <h1>Map Component</h1>
+        <h1>Hello this is VoyageDetailsContainer for {this.state.voyageId}</h1>
       </div>
     );
   }
