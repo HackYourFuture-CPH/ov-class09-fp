a151ab217f9879439d9f7a7fdd820c4cbeb0d9c5
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-18 23:42:21 +0200

    Styling Voyages Details

--- src/client/components/VoyageDetails.js
+++ src/client/components/VoyageDetails.js
@@ -6,7 +6,6 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
