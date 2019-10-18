812159772aea9cdcafdb8b8c7ad6851be9963c13
justinaoyov <justina.oyovwevotu@gmail.com>
2019-10-18 14:15:24 +0200

    Voyage Deteails Container - WIP

--- src/client/components/VoyageDetailsContainer.js
+++ src/client/components/VoyageDetailsContainer.js
@@ -1,5 +1,95 @@
-import React from "react";
+import React, { Component } from "react";
+import VoyageDetails from "./VoyageDetails";
+import { getTokenData, getAuthToken } from "../utilities/getTokenData";
+import axios from "axios";
+import camelcaseKeys from "camelcase-keys";
+import MapComponent from "./MapComponent";
+import Marker from "./Marker";
 
-export default function VoyageDetailsContainer({ match }) {
-  return <div>This page is about the Voyage ID = {match.params.voyage_id}</div>;
+export default class VoyageDetailsContainer extends Component {
+  state = {
+    // Get auth token from localstorage
+    organizationId: getTokenData("organization_id"),
+    voyages: [],
+    vesselReports: [],
+    suggestedRoutes: []
+  };
+
+  // Get Voyages by organization. Response will be camelCased. Returns Promise.
+  getVoyages(organizationId) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`/api/organizations/${organizationId}/voyages?status=ongoing`, {
+          headers: { authorization: getAuthToken() }
+        })
+        .then(response => {
+          resolve(camelcaseKeys(response.data.voyages));
+        })
+        .catch(error => reject(error));
+    });
+  }
+
+  async getVesselReport(voyageId) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(
+          `/api/voyages/${voyageId}/vessel-reports?orderBy=desc&limit=1&offset=0`,
+          {
+            headers: { authorization: getAuthToken() }
+          }
+        )
+        .then(response => {
+          resolve(camelcaseKeys(response.data)); // get all vessel reports
+        })
+        .catch(error => reject(error));
+    });
+  }
+
+  componentDidMount() {
+    // Populate state from Voyages
+    this.getVoyages(this.state.organizationId)
+      .then(voyages => {
+        this.setState({ voyages });
+      })
+      .then(() => {
+        // Populate state from Vessel Reports
+        this.state.voyages.map(voyage => {
+          this.getVesselReport(voyage.id).then(vesselReport => {
+            this.setState({
+              vesselReports: [...this.state.vesselReports, vesselReport]
+            });
+          });
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>This page is about the Voyage ID = {match.params.voyage_id}</div>;
+        <MapComponent
+          vesselReports={this.state.vesselReports}
+          suggestedRoutes={this.state.suggestedRoutes}
+          options={mapOptions}
+        />
+      </div>
+    );
+  }
 }
+
+// Configuration object for Map
+const mapOptions = {
+  centerMapCoordinates: [12.5244140625, 55.640398956687356],
+  zoom: 1,
+  style: {
+    color: {
+      suggestedRoute: "red",
+      elapsedRoute: "blue"
+    },
+    marker: {
+      markerComponent: Marker,
+      defaultSize: "sm",
+      selectedSize: "md"
+    }
+  }
+};
