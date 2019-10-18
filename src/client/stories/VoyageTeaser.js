f04821c9435d352c639e46a119d0f7cb3bd19844
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-18 20:16:20 +0200

    correcting props ;)

--- src/client/stories/VoyageTeaser.js
+++ src/client/stories/VoyageTeaser.js
@@ -5,16 +5,16 @@ export function VoyageTeaserStory() {
   const vesselName = text("Vessel Name", "Yeniser River");
   const departureFrom = text("Departure From", "Sabetta");
   const arrivesAt = text("Arrives At", "Rotterdam");
-  const etd = text("ETD", "2012-08-17 21:18");
-  const eta = text("ETA", "2012-08-24 06:43");
+  const departure_time = text("ETD", "2012-08-17 21:18");
+  const target_arrival_time = text("ETA", "2012-08-24 06:43");
 
   return (
     <VoyageTeaser
       vesselName={vesselName}
       departureFrom={departureFrom}
       arrivesAt={arrivesAt}
-      etd={etd}
-      eta={eta}
+      departure_time={departure_time}
+      target_arrival_time={target_arrival_time}
     />
   );
 }
