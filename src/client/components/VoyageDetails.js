fa0be9a5a2b601e05254bbc12a3140927ddb5b87
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-18 23:28:07 +0200

    Styling Voyages Details

--- src/client/components/VoyageDetails.js
+++ src/client/components/VoyageDetails.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
-import DataCard from "./DataCard";
-import { CardGroup, CardGroupHeader } from "./CardGroup";
 import VoyageTeaser from "./VoyageTeaser";
 import PropTypes from "prop-types";
+
+// import material-ui
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+// CSS
+const useStyles = makeStyles(theme => ({
+  card: { display: "inline-block", backgroundColor: "#D9D9D9", minWidth: 300 },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  infoTitle: {
+    color: "#595959",
+    fontSize: "0.9rem"
+  },
+  describeText: {
+    color: "#262626",
+    fontSize: "1.2rem"
+  }
+}));
 
 function VoyageDetails({
   vesselName,
@@ -16,8 +49,14 @@ function VoyageDetails({
   hsfo,
   ulsfo
 }) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
-    <div>
+    <Card className={classes.card}>
       <VoyageTeaser
         vesselName={vesselName}
         departureFrom={departsFrom}
@@ -25,21 +64,43 @@ function VoyageDetails({
         departure_time={departure_time}
         target_arrival_time={target_arrival_time}
       />
-
-      <CardGroupHeader title="Position" />
-      <CardGroup>
-        <DataCard title="Date" data={date} />
-        <DataCard title="Latitude" data={latitude} />
-        <DataCard title="Longitude" data={longitude} />
-      </CardGroup>
-
-      <CardGroupHeader title="Consumption" />
-      <CardGroup>
-        <DataCard title="HSFO" data={hsfo} />
-        <DataCard title="ULSFO" data={ulsfo} />
-        <DataCard title="FOC Total" data={hsfo + ulsfo} />
-      </CardGroup>
-    </div>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography className={classes.infoTitle}>Position</Typography>
+          <Typography className={classes.describeText}>
+            Date : {date}
+          </Typography>
+          <Typography className={classes.describeText}>
+            Latitude : {latitude}
+          </Typography>
+          <Typography className={classes.describeText}>
+            Longitude : {longitude}
+          </Typography>
+          <Typography className={classes.infoTitle}>Consumption</Typography>
+          <Typography className={classes.describeText}>
+            HSFO : {hsfo}
+          </Typography>
+          <Typography className={classes.describeText}>
+            ULSGO : {ulsfo}
+          </Typography>
+          <Typography className={classes.describeText}>
+            Total : {hsfo + ulsfo}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
 }
 
