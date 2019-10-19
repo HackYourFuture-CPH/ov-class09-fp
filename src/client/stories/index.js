975ecff9ffaab4c9a18e29c999e5e4a8c615145b
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-19 15:31:33 +0200

    Improving index in storyBook

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -47,12 +47,6 @@ const VoyageStories = storiesOf("Start Voyage", module).add(
 );
 VoyageStories.addDecorator(withKnobs);
 
-const VoyageDetailsStories = storiesOf("VoyageDetails", module).add(
-  "VoyageDetails",
-  VoyageDetailsStory
-);
-VoyageDetailsStories.addDecorator(withKnobs);
-
 const CardStories = storiesOf("Card", module)
   .add("DataCard", DataCardStory)
   .add("CardGroup", CardGroupStory)
@@ -60,8 +54,9 @@ const CardStories = storiesOf("Card", module)
 CardStories.addDecorator(withKnobs);
 
 const VoyagesListStories = storiesOf("Voyages", module)
-  .add("VoyageList", VoyageListStory)
-  .add("VoyageTeaser", VoyageTeaserStory);
+  .add("VoyageTeaser", VoyageTeaserStory)
+  .add("VoyageDetails", VoyageDetailsStory)
+  .add("VoyageList", VoyageListStory);
 VoyagesListStories.addDecorator(withKnobs);
 
 const SuggestedRouteSummaryStories = storiesOf("Suggested Routes", module)
