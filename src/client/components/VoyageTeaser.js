98e3cef66daab7306cbba77aaeb268de2e803af5
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-19 15:13:58 +0200

    Improving VoyageTeaser Style ;)

--- src/client/components/VoyageTeaser.js
+++ src/client/components/VoyageTeaser.js
@@ -8,16 +8,16 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
 
 //CSS Colors: #F2F2F2,#D9D9D9,#595959,#262626,#0D0D0D
-const voyage_teaser = makeStyles({
+const useStyles = makeStyles({
   card: {
-    display: "inline-block",
-    backgroundColor: "#F2F2F2",
-    minWidth: 300
-  },
-  summery: {
-    display: "inline-block"
+    minWidth: 300,
+    marginTop: "0.75rem",
+    marginRight: "0.75rem",
+    marginLeft: "0.75rem"
   },
   headerTitle: {
     color: "#0D0D0D",
@@ -30,6 +30,14 @@ const voyage_teaser = makeStyles({
   describeText: {
     color: "#262626",
     fontSize: "1.2rem"
+  },
+  button: {
+    fontSize: "1.25rem",
+    fontWeight: "bold",
+    color: "#D9D9D9"
+  },
+  actionCard: {
+    float: "right"
   }
 });
 
@@ -41,32 +49,36 @@ function VoyageTeaser({
   departure_time,
   target_arrival_time
 }) {
-  const classes = voyage_teaser();
+  const classes = useStyles();
 
   return (
     <Card className={classes.card}>
+      <CardHeader className={classes.headerTitle} title={vesselName} />
       <CardContent>
-        <Typography className={classes.headerTitle}>{vesselName}</Typography>
-        <CardContent className={classes.summery}>
-          <Typography className={classes.infoTitle}>DEPARTURE</Typography>
-          <Typography className={classes.describeText}>
-            {departureFrom}
-          </Typography>
-          <Typography className={classes.describeText}>
-            {departure_time}
-          </Typography>
-        </CardContent>
-        <CardContent className={classes.summery}>
-          <Typography className={classes.infoTitle}>ETA</Typography>
-          <Typography className={classes.describeText}>{arrivesAt}</Typography>
-          <Typography className={classes.describeText}>
-            {target_arrival_time}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Show Details</Button>
-        </CardActions>
+        <Grid container>
+          <Grid item xs={6} sm={3}>
+            <Typography className={classes.infoTitle}>DEPARTURE</Typography>
+            <Typography className={classes.describeText}>
+              {departureFrom}
+            </Typography>
+            <Typography className={classes.describeText}>
+              {departure_time}
+            </Typography>
+          </Grid>
+          <Grid itemxs={6} sm={3}>
+            <Typography className={classes.infoTitle}>ETA</Typography>
+            <Typography className={classes.describeText}>
+              {arrivesAt}
+            </Typography>
+            <Typography className={classes.describeText}>
+              {target_arrival_time}
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
+      <CardActions className={classes.actionCard}>
+        <Button className={classes.button}>Show Details</Button>
+      </CardActions>
     </Card>
   );
 }
