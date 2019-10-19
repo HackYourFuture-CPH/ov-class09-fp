7f7d715d00e9699a9ef31d2c70a7241b6e420797
Rahim <ismailirahim6@gmail.com>
2019-10-19 13:07:48 +0200

    deleted console log from suggestedRouteSummary

--- src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
+++ src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
@@ -12,7 +12,7 @@ export default function SuggestedRouteSummary({
   const selectRouteBtn = () => {
     handleClick(id);
   };
-  console.log("this is selected", isSelected);
+
   return (
     <div>
       <>
@@ -30,28 +30,6 @@ export default function SuggestedRouteSummary({
           </>
         )}
       </>
-
-      {/* {isSelected ? (
-        <>
-          <Field title="DURATION" description={duration} />
-          <Field title="TOTAL COST" description={`$${price}`} />
-          <button onClick={selectRouteBtn}>SELECT ROUTE </button>
-        </>
-      ) : (
-        <>
-          <h2>Selected Route</h2>
-          <Field title="DURATION" description={duration} />
-          <Field title="TOTAL COST" description={`$${price}`} />
-        </>
-      )} */}
-
-      {/* {isSelected ? <h4>SELECTED ROUTE</h4>
-      <Field title="DURATION" description={duration} />
-      <Field title="TOTAL COST" description={`$${price}`} /> : null}
-      
-      {!isSelected ? (
-        <button onClick={selectRouteBtn}>SELECT ROUTE BTNNNNNNNN</button>
-      ) : null} */}
     </div>
   );
 }
