66b251f6b0fda858594b0b3ca6b7bced7efa89c1
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-19 15:12:45 +0200

    Import CSS to App.js

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import MainRouter from "./routes/main-router";
 
+//import CSS
+import "./App.css";
+
 class App extends Component {
   render() {
     return (
