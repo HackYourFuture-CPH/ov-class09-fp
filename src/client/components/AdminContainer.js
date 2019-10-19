20d887a8ea9c29f43f5b4d7cfd60381df1bcf13c
Kurdator <a-hassam@outlook.com>
2019-10-19 12:58:40 +0200

    CreateUserButton has been added to AdminContainer

--- src/client/components/AdminContainer.js
+++ src/client/components/AdminContainer.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import CreateUserButton from "./CreateUserButton";
 
 export default class AdminContainer extends Component {
+  state = {
+    name: "",
+    email: "",
+    organizationName: ""
+  };
+
+  organizationOptions = [
+    "D'Amore, Miller and Kemmer",
+    "Ledner and Sons",
+    "Cummerata, McClure and Koch"
+  ];
+
+  handleCreate = event => {
+    event.preventDefault();
+    //this.users.push(this.state);
+    console.log(this.state);
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
-    return <div>This is the Admin Page ;)</div>;
+    return (
+      <div>
+        <div>This is the Admin Page ;)</div>
+        <CreateUserButton
+          state={this.state}
+          handleChange={this.handleChange}
+          handleCreate={this.handleCreate}
+          organizationOptions={this.organizationOptions}
+        />
+      </div>
+    );
   }
 }
