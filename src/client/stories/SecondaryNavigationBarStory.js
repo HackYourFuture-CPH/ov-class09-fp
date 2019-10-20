fc2935dc048076193d94c1941c275619223a52f5
fawadik <fawadik@yahoo.com>
2019-10-20 12:57:55 +0200

    pulled new changes

--- src/client/stories/SecondaryNavigationBarStory.js
+++ src/client/stories/SecondaryNavigationBarStory.js
@@ -1,14 +1,14 @@
 import React from "react";
 import SecondaryNavigationBar from "../components/SecondaryNavigationBar";
 import { withKnobs, text } from "@storybook/addon-knobs";
-import { getTokenData } from "../utilities/getTokenData";
+//import { getTokenData } from "../utilities/getTokenData";
 
 export function SecondaryNavigationBarStory() {
   const userName = text("User Name", "Fowad");
   const userRole = text("User Role", "admin");
 
-  let userData = getTokenData();
-  console.log("This is", userData);
+  //let userData = getTokenData();
+
   return (
     <div>
       <SecondaryNavigationBar userName={userName} userRole={userRole} />
