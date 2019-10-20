e566141b23594c44a2a050aeaeea623e676a5064
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-10-21 00:47:28 +0200

    removed useStyle

--- src/client/components/VoyageDetailsContainer.js
+++ src/client/components/VoyageDetailsContainer.js
@@ -129,7 +129,7 @@ export default class VoyageDetailsContainer extends Component {
         }
       }
     };
-    const classes = useStyles();
+
     return (
       <>
         <Grid container spacing={1}>
