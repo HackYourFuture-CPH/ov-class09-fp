e1862958fe47a4ee56c75541edb0e63b673d346c
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-20 17:18:58 +0200

    Styling Suggested Route Summary

--- src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
+++ src/client/components/SuggestedRoutes/SuggestedRouteSummary.js
@@ -1,6 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Field from "../Field";
+
+//import material ui
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275
+  },
+  cardHeader: {
+    color: "red"
+  },
+  infoText: {
+    color: "blue",
+    fontSize: "0.9rem",
+    marginBottom: "5px",
+    paddingLeft: "16px"
+  },
+  describeText: {
+    color: "yellow",
+    fontSize: "1.2rem",
+    marginBottom: "30px",
+    paddingLeft: "16px"
+  }
+});
 
 export default function SuggestedRouteSummary({
   id,
@@ -9,28 +36,36 @@ export default function SuggestedRouteSummary({
   duration,
   price
 }) {
+  const classes = useStyles();
+
   const selectRouteBtn = () => {
     handleClick(id);
   };
 
   return (
-    <div>
-      <>
-        {!isSelected ? (
-          <>
-            <Field title="DURATION" description={duration} />
-            <Field title="TOTAL COST" description={`$${price}`} />
-            <button onClick={selectRouteBtn}>SELECT ROUTE </button>
-          </>
-        ) : (
-          <>
-            <h2>Selected Route</h2>
-            <Field title="DURATION" description={duration} />
-            <Field title="TOTAL COST" description={`$${price}`} />
-          </>
-        )}
-      </>
-    </div>
+    <Card className={classes.card}>
+      {!isSelected ? (
+        <CardContent>
+          <Typography className={classes.infoText}>DURATION</Typography>
+          <Typography className={classes.describeText}>{duration}</Typography>
+          <Typography className={classes.infoText}>TOTAL COST</Typography>
+          <Typography
+            className={classes.describeText}
+          >{`$${price}`}</Typography>
+          <button onClick={selectRouteBtn}>SELECT ROUTE </button>
+        </CardContent>
+      ) : (
+        <CardContent>
+          <CardHeader className={classes.cardHeader} title="SELECTED ROUTES" />
+          <Typography className={classes.infoText}>DURATION</Typography>
+          <Typography className={classes.describeText}>{duration}</Typography>
+          <Typography className={classes.infoText}>TOTAL COST</Typography>
+          <Typography
+            className={classes.describeText}
+          >{`$${price}`}</Typography>
+        </CardContent>
+      )}
+    </Card>
   );
 }
 
