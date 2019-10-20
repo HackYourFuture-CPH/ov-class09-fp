369d50cc2c89ce4594fec3090d2e501e871b08c1
lil2400 <il.memedi@gmail.com>
2019-10-20 14:22:17 +0200

    Added component to App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import MainRouter from "./routes/main-router";
-
+import NavBar from "./components/NavBar";
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <MainRouter />
       </div>
     );
