1c98e781a497e22d12360dad0fd7fdfafe2e4497
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-10-20 17:18:01 +0200

    Suggested Routes Summary in story is corrected!

--- src/client/stories/suggestedRoutes/suggestedRouteSummary.js
+++ src/client/stories/suggestedRoutes/suggestedRouteSummary.js
@@ -1,14 +1,14 @@
 import React from "react";
 import SuggestedRouteSummary from "../../components/SuggestedRoutes/SuggestedRouteSummary";
-import { number, boolean } from "@storybook/addon-knobs";
+import { number, boolean, text } from "@storybook/addon-knobs";
 
 export function suggestedRouteSummary() {
   return (
     <SuggestedRouteSummary
       id={number("id", 1)}
-      time={number("Time", 24)}
+      duration={text("Time", "24h")}
       price={number("Price", 104)}
-      isSelected={(boolean("Is Selected"), false)}
+      isSelected={boolean("Is Selected", true)}
       handleClick={() => console.log("It Works ;)")}
     />
   );
