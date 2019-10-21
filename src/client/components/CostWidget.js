00180ea8f50703623455bc6483ce271f0fee6968
Swapna-Sahu <47104798+Swapna-Sahu@users.noreply.github.com>
2019-10-21 22:31:37 +0200

    Material ui voyage details container (#162)
    * added material-UI
    
    * removed storybook for vesselDetailsPage
    
    * removed useStyle
    
    * removed card container from voyage teaser
    
    * Improve checking for non-set selected routes

--- src/client/components/CostWidget.js
+++ src/client/components/CostWidget.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 import DataCard from "./DataCard";
 import { CardGroup, CardGroupHeader } from "./CardGroup";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 300,
+    marginTop: "0.75rem",
+    marginRight: "0.75rem",
+    marginLeft: "0.75rem"
+  },
+  headerTitle: {
+    color: "#0D0D0D",
+    fontSize: "1.5rem"
+  },
+  infoTitle: {
+    color: "#595959",
+    fontSize: "0.9rem"
+  },
+  describeText: {
+    color: "#262626",
+    fontSize: "1.2rem"
+  },
+  button: {
+    fontSize: "1.25rem",
+    fontWeight: "bold",
+    color: "#0B3C61"
+  },
+  actionCard: {
+    float: "right"
+  }
+});
 
 function CostWidget({
   totalCost,
@@ -12,16 +47,35 @@ function CostWidget({
 }) {
   const currentCost = hfoCost * hfoConsumption + lsfoCost * lsfoConsumption;
   const remainingCost = totalCost - currentCost;
-
+  const classes = useStyles();
   return (
-    <div>
-      <CardGroupHeader title="Cost Widget" />
-      <CardGroup>
-        <DataCard title="Total Cost" data={totalCost} />
-        <DataCard title="Current Cost" data={currentCost} />
-        <DataCard title="Remaining Cost" data={remainingCost} />
-      </CardGroup>
-    </div>
+    <Card className={classes.card}>
+      <CardHeader className={classes.headerTitle} title="Cost Widget" />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6} sm={3}>
+            <Typography className={classes.infoTitle}>Total Cost</Typography>
+            <Typography className={classes.describeText}>
+              {totalCost}{" "}
+            </Typography>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Typography className={classes.infoTitle}>Current Cost</Typography>
+            <Typography className={classes.describeText}>
+              {currentCost}{" "}
+            </Typography>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Typography className={classes.infoTitle}>
+              Remaining Cost
+            </Typography>
+            <Typography className={classes.describeText}>
+              {remainingCost}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
 
--- src/client/components/SuggestedRouteTable.js
+++ src/client/components/SuggestedRouteTable.js
@@ -6,37 +6,79 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 300,
+    marginTop: "0.75rem",
+    marginRight: "0.75rem",
+    marginLeft: "0.75rem"
+  },
+  headerTitle: {
+    color: "#0D0D0D",
+    fontSize: "1.5rem"
+  },
+  infoTitle: {
+    color: "#595959",
+    fontSize: "0.9rem"
+  },
+  describeText: {
+    color: "#262626",
+    fontSize: "1.2rem"
+  },
+  button: {
+    fontSize: "1.25rem",
+    fontWeight: "bold",
+    color: "#0B3C61"
+  },
+  bottom: {
+    marginTop: 10,
+    paddingLeft: 200
+  }
+});
 
 const SuggestedRouteTable = ({ data, tableNames }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h3>SELECTED ROUTE</h3>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>{tableNames[0]}</TableCell>
-            <TableCell align="right">{tableNames[1]}</TableCell>
-            <TableCell align="right">{tableNames[2]}</TableCell>
-            <TableCell align="right">{tableNames[3]}</TableCell>
-            <TableCell align="right">{tableNames[4]}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.createdAt}
-              </TableCell>
-              <TableCell align="right">{row.latitude}</TableCell>
-              <TableCell align="right">{row.longitude}</TableCell>
-              <TableCell align="right">{row.speed}</TableCell>
-              <TableCell align="right">{row.rpm}</TableCell>
+    <Card className={classes.card}>
+      <CardHeader className={classes.headerTitle} title="SELECTED ROUTE" />
+      <CardContent>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>{tableNames[0]}</TableCell>
+              <TableCell align="right">{tableNames[1]}</TableCell>
+              <TableCell align="right">{tableNames[2]}</TableCell>
+              <TableCell align="right">{tableNames[3]}</TableCell>
+              <TableCell align="right">{tableNames[4]}</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <h4>Currently optimizing for the cheapest possible route.</h4>
-    </div>
+          </TableHead>
+          <TableBody>
+            {data.map(row => (
+              <TableRow key={row.key}>
+                <TableCell component="th" scope="row">
+                  {row.createdAt}
+                </TableCell>
+                <TableCell align="right">{row.latitude}</TableCell>
+                <TableCell align="right">{row.longitude}</TableCell>
+                <TableCell align="right">{row.speed}</TableCell>
+                <TableCell align="right">{row.rpm}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Typography variant="h6" component="h6" className={classes.bottom}>
+          Currently optimizing for the cheapest possible route.
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
